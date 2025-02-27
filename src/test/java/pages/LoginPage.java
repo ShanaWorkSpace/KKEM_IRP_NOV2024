b9f37f735ab706d2b416c54722fea5c453e422ae
Shana Yasmin S <shanayasminofficial@gmail.com>
2025-02-27 13:56:08 +0530

    exam

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -7,60 +7,76 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.LocatorsReader;
 
-public class LoginPage extends LocatorsReader{
-	WebDriver driver;
-	
-	public LoginPage(WebDriver driver) throws IOException {
-		
-		this.driver=driver;
-	}
-	
-	 
-	public boolean isCaptchaPresent() {
-		 WebElement cap = driver.findElement(By.xpath(propi.getProperty("loginPage.captcha")));
-		 System.out.println(cap.getText());
-		return true;
-	 }
-	
-	
-	
-	public void enterCaptchaManually() {
+public class LoginPage extends LocatorsReader {
+    
+    // WebDriver instance
+    WebDriver driver;
+    
+    // Constructor to initialize the WebDriver
+    public LoginPage(WebDriver driver) throws IOException {
+        this.driver = driver;
+    }
+
+    /**
+     * Checks if the CAPTCHA is present on the login page.
+     * @return true if CAPTCHA is detected, false otherwise.
+     */
+    public boolean isCaptchaPresent() {
+        WebElement cap = driver.findElement(By.xpath(propi.getProperty("loginPage.captcha")));
+        System.out.println(cap.getText());
+        return true; // This should ideally return cap.isDisplayed() instead of always returning true
+    }
+
+    /**
+     * Provides a manual pause for CAPTCHA entry.
+     * It waits for a predefined time to allow the user to solve the CAPTCHA manually.
+     */
+    public void enterCaptchaManually() {
         System.out.println("CAPTCHA detected! Please solve it manually.");
         try {
             Thread.sleep(15000); // Wait for manual input (Adjust as needed)
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Searches for an entity using the provided search term.
+     * @param need - The search term to be entered in the search box.
+     */
+    public void searchEntity(String need) {
+        WebElement search = driver.findElement(By.xpath(propi.getProperty("loginPage.search")));
+        search.sendKeys(need);
         
-  
-}
-	
-	public void searchEntity (String need) {
-		WebElement search = driver.findElement(By.xpath(propi.getProperty("loginPage.search")));
-		search.sendKeys(need);
-		WebElement searchb = driver.findElement(By.xpath(propi.getProperty("loginPage.searchbutton")));
-		searchb.click();
-		
-	}
-	
-	public void ExploreItem() {
-		WebElement item = driver.findElement(By.cssSelector(propi.getProperty("searchPage.Obj")));
-		if (item.isDisplayed()) {
+        WebElement searchb = driver.findElement(By.xpath(propi.getProperty("loginPage.searchbutton")));
+        searchb.click();
+    }
+
+    /**
+     * Clicks on an item from the search results.
+     * If no item is found, it prints an appropriate message.
+     */
+    public void ExploreItem() {
+        WebElement item = driver.findElement(By.cssSelector(propi.getProperty("searchPage.Obj")));
+        if (item.isDisplayed()) {
             item.click();
             System.out.println("Item clicked successfully.");
         } else {
             System.out.println("No items found in search results.");
         }
-	}
-	public void Addtem() {
+    }
 
-		WebElement add = driver.findElement(By.xpath(propi.getProperty("ProductPage.add")));
-		if (add.isDisplayed()) {
+    /**
+     * Adds an item to the cart if the "Add to Cart" button is present.
+     * If the button is not found, it prints an appropriate message.
+     */
+    public void Addtem() {
+        WebElement add = driver.findElement(By.xpath(propi.getProperty("ProductPage.add")));
+        if (add.isDisplayed()) {
             add.click();
             System.out.println("Item added to the cart.");
         } else {
             System.out.println("Add to Cart button not found.");
         }
-	}
-	
+    }
 }
--- src/test/java/testcases/LoginPageTest.java
+++ src/test/java/testcases/LoginPageTest.java
@@ -2,7 +2,6 @@ package testcases;
 
 import java.io.IOException;
 
-
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,45 +10,48 @@ import base.TestBase;
 import constants.AutomationConstants;
 import pages.LoginPage;
 
-public class LoginPageTest extends TestBase{
-		
-		LoginPage log;
-		
-		@BeforeClass
-		public void page_init() throws IOException {
-			log = new LoginPage(driver);
-			// Check if CAPTCHA is present
-	        if (log.isCaptchaPresent()) {
-	            log.enterCaptchaManually();
-	        }
-		}
-		
-		@Test(priority = 1)
-		public void testURL() {
-			String currentUrl=driver.getCurrentUrl();
-		    Assert.assertEquals(currentUrl, AutomationConstants.loginURL);
-		    System.out.println("Test:URL Verification passed");
-		    }
-		
-		@Test(priority=2)
-		public void search() {
-			log.searchEntity(prop.getProperty("Need"));	        
-		    System.out.println("Test:First Search Verification passed");
-		    }
-		
-		@Test(priority=3)
-		public void explore() {
-			log.ExploreItem();	
-			
-		    System.out.println("Test:Item Search Verification passed");
-		    }
-		
-		@Test(priority=3)
-		public void add() {
-			log.Addtem();	
-		    System.out.println("Test:Item Add Verification passed");
-		    }
-		
-		
-		
-				}
+public class LoginPageTest extends TestBase {
+    
+    // Instance variable for LoginPage
+    LoginPage log;
+    
+    @BeforeClass
+    public void page_init() throws IOException {
+        // Initialize the LoginPage object
+        log = new LoginPage(driver);
+        
+        // Check if CAPTCHA is present and handle it manually
+        if (log.isCaptchaPresent()) {
+            log.enterCaptchaManually();
+        }
+    }
+    
+    @Test(priority = 1)
+    public void testURL() {
+        // Get the current URL and verify if it matches the expected URL
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertEquals(currentUrl, AutomationConstants.loginURL);
+        System.out.println("Test: URL Verification passed");
+    }
+    
+    @Test(priority = 2)
+    public void search() {
+        // Perform a search operation using the "Need" property from configuration
+        log.searchEntity(prop.getProperty("Need"));
+        System.out.println("Test: First Search Verification passed");
+    }
+    
+    @Test(priority = 3)
+    public void explore() {
+        // Explore an item on the page
+        log.ExploreItem();
+        System.out.println("Test: Item Search Verification passed");
+    }
+    
+    @Test(priority = 3)
+    public void add() {
+        // Add an item
+        log.Addtem();
+        System.out.println("Test: Item Add Verification passed");
+    }
+}
